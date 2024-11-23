e6109fe39804a09b33dc68a7ff696fd6f63bf9a2
Sor4nex <aleksei.ivanovv11@gmail.com>
2024-11-23 13:33:22 +0300

    feat: viewsets were replaced with generic APIViews for better flexibility. Permissions changes: now all posts can be seen by by any people, but concrete posts only for authenicated. Updating and deleting only for authors or admins

--- vunkpunk/vp_users/permissions.py
+++ vunkpunk/vp_users/permissions.py
@@ -1,6 +1,16 @@
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 
 
 class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return request.user.is_authenticated
         return request.user == obj.user
+
+
+class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
--- vunkpunk/vp_users/views.py
+++ vunkpunk/vp_users/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import generics, permissions, viewsets
 from vp_users.models import User
 from vp_users.serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserRetrieveDestroyView(generics.RetrieveDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.IsAuthenticated(), )
+        return (permissions.IsAdminUser(), )
