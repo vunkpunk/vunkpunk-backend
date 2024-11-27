88a206a453e0b29c4102a53b555d9ed5f1de7e95
wrdx <wrdxwrdxwrdx@gmail.com>
2024-11-27 16:39:10 +0300

    fix: add new ip

--- vunkpunk/vunkpunk/settings.py
+++ vunkpunk/vunkpunk/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list[str] = ["127.0.0.1"]
+ALLOWED_HOSTS: list[str] = ["127.0.0.1", "10.0.2.2"]
 
 
 # Application definition
